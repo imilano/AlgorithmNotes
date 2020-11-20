c8436b5321b2e85e29fa88bf76337a7ab338da5a
imilano <m15752836560_1@163.com>
2020-11-20 19:50:42 +0800

    [Update] Add 89,93,97,99

---
+++ LeetCode/leet_89/gray_code.go
@@ -0,0 +1,20 @@
+package leet_89
+
+import "math"
+
+/*
+The gray code is a binary numeral system where two successive values differ in only one bit.
+
+Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+ */
+
+//也就是将十进制转换为格雷码，参见维基百科。
+
+func grayCode(n int) []int {
+	var res []int
+	for i := 0; i< int(math.Pow(2,float64(n)));i++ {
+		res = append(res, (i >> 1)^i)
+	}
+
+	return res
+}
---
+++ LeetCode/leet_93/restore_ip_address.go
@@ -0,0 +1,79 @@
+package leet_93
+
+import "strconv"
+
+/*
+Given a string s containing only digits, return all possible valid IP addresses that can be obtained from s.
+You can return them in any order.
+A valid IP address consists of exactly four integers, each integer is between 0 and 255, separated by single dots and cannot have
+leading zeros. For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses and "0.011.255.245", "192.168.1.312" and "192.168@1.1" are invalid IP addresses.
+*/
+
+//--------------------------------------------
+// Iterative
+func restoreIpAddresses(s string) []string {
+	var res []string
+	length := len(s)
+	if length == 0 {
+		return res
+	}
+
+	for i := 1; i < 4 && i< length-2;i++ {
+		for j := i+1; j < i  + 4 && j< length-1;j++ {
+			for k := j+1; k < j+4 && k < length; k++ {
+				s1,s2,s3,s4 := s[0:i],s[i:j],s[j:k],s[k:length]
+				if isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4) {
+					res = append(res,s1+"."+s2+"."+s3+"."+s4)
+				}
+			}
+		}
+	}
+
+	return res
+}
+
+func isValid(s string) bool {
+	if len(s) > 3 || len(s) == 0 || (s[0] == '0' && len(s) > 1) {
+		return false
+	}
+
+	p,_ := strconv.Atoi(s)
+	if p > 255 {
+		return false
+	}
+
+	return true
+}
+
+
+//-----------------------------------------------------
+func restoreIpAddresses2(s string) []string {
+	var res []string
+	length := len(s)
+	if length == 0 {
+		return res
+	}
+
+	for a := 1; a <=3;a++ {
+		for b := 1; b <= 3; b++ {
+			for c := 1; c <= 3; c++ {
+				for d := 1; d <= 3;d++ {
+					if a+b+c+d  == length {
+						if s[0:a] <= "255" && s[a:a+b] <= "255" && s[a+b:a+b+c] <= "255" && s[a+b+c:] <= "255" {
+							ss := s[0:a] + "." + s[a:a+b] + "." + s[a+b:a+b+c] + "." + s[a+b+c:]
+							if len(ss) == length + 3 {
+								res = append(res,ss )
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return res
+}
+
+
+//----------------------------------------------------
+// DFS
