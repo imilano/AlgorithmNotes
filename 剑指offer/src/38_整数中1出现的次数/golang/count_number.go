68bb7e1264e71d860966e79ae2a7ff49c6241d4e
imilano <m15752836560_1@163.com>
2021-04-07 09:53:00 +0800

    Add golang version for problems

---
+++ 剑指offer/src/38_整数中1出现的次数/golang/count_number.go
@@ -0,0 +1,7 @@
+package golang
+
+//  题目描述：输入一个整数n，求1～n这n个整数的十进制中1出现的次数。例如，输入12,1～12这些整数中包含1的数字有1、10、11、12,1一共出现5次
+func NumberOf1Between1AndN_Solution( n int ) int {
+	// write code here
+	return 0
+}
--- 剑指offer/src/61_圆圈中最后剩下的数字（约瑟夫环问题）/golang/last_remaining.go
+++ 剑指offer/src/61_圆圈中最后剩下的数字（约瑟夫环问题）/golang/last_remaining.go
@@ -6,8 +6,31 @@ package golang
 
 //问题简化：给定一个长度为n的序列，每次向后数m个元素并删除，那么最终留下的是第几个元素。这个问题有拆解为子问题的潜质：如果我们知道了n-1个元素中
 //会删除的是哪个元素，那么我们就知道n个元素中删除的是哪个元素。我们定义子问题f(n,m)=y，表示从n个元素中向后数m个元素并删除，最终剩下的数的下标是
-//y。y是下标就意味着从index=0开始数，数y+1个数
+//y。y是下标就意味着从index=0开始数，数y+1个数。
+
+//方法的核心在于：只关心最终或者的那个人的序号变化情况，从最终结果反推出规律。参见 https://dyfloveslife.github.io/2019/12/27/offer-Josephus/
+// 可得到递推公式：f(n,m) = (f(n-1,m) + m) % n. 返回条件，当n=1时，必然返回0
+// 时间复杂度O（n)，空间复杂度O(n)
 func LastRemaining_Solution( n int ,  m int ) int {
 	// write code here
-	return 0
+	if n == 0 {
+		return -1
+	}
+
+	if n == 1 {
+		return 0
+	}
+
+	x := LastRemaining_Solution(n-1,m)
+	return (x+m)%n
+}
+
+// 迭代版本
+func lastRemaining(n int, m int) int {
+	x := -1
+	for i := 1; i <n+1; i++ {
+		x = (x+m)%i
+	}
+
+	return x
 }
